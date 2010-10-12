3a9f8a606b1a6dd92687976d0b55e4529d89f012
Philip Lewis <pcl@pclewis.com>
2010-10-12 00:08:01 -0400

    more palette experiments..a little better but still terrible

--- newcode/src/at.java
+++ newcode/src/at.java
@@ -15,7 +15,10 @@ public class at extends z {
 	int tileWidth;
 	int tileHeight;
 	int[] palette;
+	boolean[] paletteDir;
+	float[][] paletteHSB;
 	int[] data;
+	private static final float STEP = 0.01f;
 
 	public at(Minecraft game) {
 		super(ly.D.bb);
@@ -42,28 +45,15 @@ public class at extends z {
 			}
 			Integer[] colors = new Integer[palBuilder.size()];
 			palBuilder.keySet().toArray(colors);
-			Arrays.sort(colors, new java.util.Comparator<Integer>() {
-				public int compare(Integer o1, Integer o2) {
-					float[] hsb1 = getHSB(o1);
-					float[] hsb2 = getHSB(o2);
-					for(int i = 2; i>=0;--i) {
-					if(hsb1[i]<hsb2[i])
-						return -1;
-					else if (hsb1[i]>hsb2[i])
-						return 1;
-					}
-					return 0;
-				}
-			});
-			palette = new int[colors.length*2-2];
+			palette = new int[colors.length];
+			paletteDir = new boolean[colors.length];
+			paletteHSB = new float[colors.length][3];
 			int p = 0;
 			for(int c : colors) {
-				System.out.println(String.format("%d:%x (%d)", p, c, lightness(c)));
 				palette[p] = c;
+				getHSB(c, paletteHSB[p]);
+				paletteDir[p] = (paletteHSB[p][2] > 0.5);
 				palBuilder.put(c, p++);
-			}
-			for(int q = p-2; q > 0; --q) {
-				palette[p++] = palette[q];
 			}
 			for(int i = 0; i < imageBuf.length; ++i) {
 				int v = imageBuf[i];
@@ -76,9 +66,26 @@ public class at extends z {
 	}
 
 	public void a() {
-		int temp = palette[0];
-		System.arraycopy(palette, 1, palette, 0, palette.length-1);
-		palette[palette.length-1] = temp;
+		for(int i = 0; i < palette.length; ++i) {
+			float[] hsb = paletteHSB[i];
+			if(paletteDir[i]) {
+				float maxS = (hsb[1] < 0.5) ? 0.5F : 1.0F;
+				float maxB = (hsb[2] < 0.5) ? 0.5F : 1.0F;
+				//hsb[1] = Math.min(hsb[1] + STEP, maxS);
+				hsb[2] = Math.min(hsb[2] + STEP, maxB);
+				if(hsb[1] >= maxS || hsb[2] >= maxB)
+					paletteDir[i] = false;
+			} else {
+				float minS = (hsb[1] < 0.5) ? 0.0F : 0.5F;
+				float minB = (hsb[2] < 0.5) ? 0.0F : 0.5F;
+				//hsb[1] = Math.max(hsb[1] - STEP, minS);
+				hsb[2] = Math.max(hsb[2] - STEP, minB);
+				if(hsb[1] <= minS || hsb[2] <= minB)
+					paletteDir[i] = true;
+			}
+			//paletteHSB[i] = hsb;
+			palette[i] = (palette[i] & 0xFF) | (Color.HSBtoRGB(hsb[0], hsb[1], hsb[2]) << 8);
+		}
 		for(int i = 0; i < data.length; ++i) {
 			int v = palette[this.data[i]];
 			this.a[(i*4)+0] = (byte)((v>>24)&0xFF);
@@ -95,10 +102,10 @@ public class at extends z {
 		return (Math.max(Math.max(r,g),b)+Math.min(Math.min(r,g),b))/2;
 	}
 
-	public static float[] getHSB(int v) {
+	public static float[] getHSB(int v, float[] ar) {
 		int r = (v>>24)&0xFF;
 		int g = (v>>16)&0xFF;
 		int b = (v>>8) &0xFF;
-		return Color.RGBtoHSB(r,g,b,null);
+		return Color.RGBtoHSB(r,g,b,ar);
 	}
 }
