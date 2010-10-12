5e19a6875a4355091bf93b630fb99c0e663e9229
Philip Lewis <pcl@pclewis.com>
2010-10-11 23:36:07 -0400

    some more palette rotation attempts...

--- newcode/src/at.java
+++ newcode/src/at.java
@@ -1,22 +1,25 @@
 import net.minecraft.client.Minecraft;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.Random;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class at extends z {
 	Minecraft game;
 	int tileNumber;
 	int tileWidth;
 	int tileHeight;
-	byte temp[];
-	int timer = 0;
-	private final static int MAX_TIMER=3;
-	Random rand = new Random();
+	int[] palette;
+	int[] data;
 
 	public at(Minecraft game) {
 		super(ly.D.bb);
+
 		this.game = game;
 		this.tileNumber = this.b;
 		try {
@@ -26,53 +29,76 @@ public class at extends z {
 			int tileX = tileNumber % 16 * tileWidth;
 			int tileY = tileNumber / 16 * tileHeight;
 			int imageBuf[] = new int[tileWidth * tileHeight];
+			data = new int[tileWidth * tileHeight];
 			tiles.getRGB(tileX, tileY, tileWidth, tileHeight, imageBuf, 0, tileWidth);
-			for( int i = 0; i < imageBuf.length; ++i ) {
+			HashMap<Integer,Integer> palBuilder = new HashMap<Integer,Integer>();
+
+			for(int i = 0; i < imageBuf.length; ++i) {
 				int v = imageBuf[i];
-				//ARGB -> RGBA
-				//System.out.println(String.format("%08x -> %02x", v, (byte)((v>>24)&0xFF)));
-				this.a[(i*4)+3] = (byte)((v>>24)&0xFF);
-				this.a[(i*4)+0] = (byte)((v>>16)&0xFF);
-				this.a[(i*4)+1] = (byte)((v>> 8)&0xFF);
-				this.a[(i*4)+2] = (byte)((v>> 0)&0xFF);
+				int c = ((v>>24)&0xFF) | ((v &0x00FFFFFF)<<8);
+				if(!palBuilder.containsKey(c)) {
+					palBuilder.put(c, 0);
+				}
+			}
+			Integer[] colors = new Integer[palBuilder.size()];
+			palBuilder.keySet().toArray(colors);
+			Arrays.sort(colors, new java.util.Comparator<Integer>() {
+				public int compare(Integer o1, Integer o2) {
+					float[] hsb1 = getHSB(o1);
+					float[] hsb2 = getHSB(o2);
+					for(int i = 2; i>=0;--i) {
+					if(hsb1[i]<hsb2[i])
+						return -1;
+					else if (hsb1[i]>hsb2[i])
+						return 1;
+					}
+					return 0;
+				}
+			});
+			palette = new int[colors.length*2-2];
+			int p = 0;
+			for(int c : colors) {
+				System.out.println(String.format("%d:%x (%d)", p, c, lightness(c)));
+				palette[p] = c;
+				palBuilder.put(c, p++);
+			}
+			for(int q = p-2; q > 0; --q) {
+				palette[p++] = palette[q];
+			}
+			for(int i = 0; i < imageBuf.length; ++i) {
+				int v = imageBuf[i];
+				int c = ((v>>24)&0xFF) | ((v &0x00FFFFFF)<<8);
+				data[i] = palBuilder.get(c);
 			}
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
-
-		temp = new byte[tileWidth*4];
 	}
 
 	public void a() {
-		if(++timer >= MAX_TIMER) {
-			timer = 0;
-			if(rand.nextBoolean()) {
-				if(rand.nextBoolean()) {
-					System.arraycopy(this.a, (tileHeight-1)*tileWidth*4, this.temp, 0, tileWidth*4);
-					System.arraycopy(this.a, 0, this.a, tileWidth*4, tileWidth*(tileHeight-1)*4);
-					System.arraycopy(this.temp, 0, this.a, 0, tileWidth*4);
-				} else {
-					System.arraycopy(this.a, 0, this.temp, 0, tileWidth*4);
-					System.arraycopy(this.a, tileWidth*4, this.a, 0, tileWidth*(tileHeight-1)*4);
-					System.arraycopy(this.temp, 0, this.a, (tileHeight-1)*tileWidth*4, tileWidth*4);
-				}
-			} else {
-				if(rand.nextBoolean()) { // left
-					for(int y = 0; y < tileHeight; ++y) {
-						int i = (y*tileWidth)*4;
-						byte temp = this.a[i];
-						System.arraycopy(this.a, i+4, this.a, i, (tileWidth-1)*4);
-						this.a[i+(tileWidth-1)*4] = temp;
-					}
-				} else { // right
-					for(int y = 0; y < tileHeight; ++y) {
-						int i = (y*tileWidth)*4;
-						byte temp = this.a[i+ (tileWidth-1)*4];
-						System.arraycopy(this.a, i, this.a, i+4, (tileWidth-1)*4);
-						this.a[i] = temp;
-					}
-				}
-			}
+		int temp = palette[0];
+		System.arraycopy(palette, 1, palette, 0, palette.length-1);
+		palette[palette.length-1] = temp;
+		for(int i = 0; i < data.length; ++i) {
+			int v = palette[this.data[i]];
+			this.a[(i*4)+0] = (byte)((v>>24)&0xFF);
+			this.a[(i*4)+1] = (byte)((v>>16)&0xFF);
+			this.a[(i*4)+2] = (byte)((v>> 8)&0xFF);
+			this.a[(i*4)+3] = (byte)((v>> 0)&0xFF);
 		}
+	}
+
+	public static int lightness(int v) {
+		int r = (v>>24)&0xFF;
+		int g = (v>>16)&0xFF;
+		int b = (v>>8) &0xFF;
+		return (Math.max(Math.max(r,g),b)+Math.min(Math.min(r,g),b))/2;
+	}
+
+	public static float[] getHSB(int v) {
+		int r = (v>>24)&0xFF;
+		int g = (v>>16)&0xFF;
+		int b = (v>>8) &0xFF;
+		return Color.RGBtoHSB(r,g,b,null);
 	}
 }
