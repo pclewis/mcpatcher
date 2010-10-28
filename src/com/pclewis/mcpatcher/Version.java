a1dcc289e45413f11cb8780271f70ab9dc513aec
Philip Lewis <pcl@pclewis.com>
2010-10-28 03:37:42 -0400

    version

---
+++ src/com/pclewis/mcpatcher/Version.java
@@ -0,0 +1,80 @@
+package com.pclewis.mcpatcher;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Version implements Comparable {
+    public static final Pattern PATTERN = Pattern.compile(
+            "(?:(\\S+) )?(\\d+)(?:\\.(\\d+)(?:\\.(\\d+)(?:_(\\d+))?)?)?"
+    );
+
+    public String variant  = null;
+    public int    major    = -1;
+    public int    minor    = -1;
+    public int    build    = -1;
+    public int    revision = -1;
+
+    public Version(String str) {
+        Matcher m = PATTERN.matcher(str);
+        if(m.find()) {
+            variant  = m.group(1);
+            major    = parseInt(m.group(2));
+            minor    = parseInt(m.group(3));
+            build    = parseInt(m.group(4));
+            revision = parseInt(m.group(5));
+        } else {
+            throw new IllegalArgumentException("Invalid version:" + str);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        if(variant != null && variant.length() > 0)
+            sb.append(variant).append(" ");
+        sb.append(major);
+        if(minor > -1) sb.append(".").append(minor);
+        if(build > -1) sb.append(".").append(build);
+        if(revision > -1) sb.append("_").append(revision);
+
+        return sb.toString();
+    }
+
+    private static int parseInt(String str) {
+        if(str == null || str.length()==0)
+            return -1;
+        return Integer.parseInt(str, 10);
+    }
+
+    public int compareTo(Object o) {
+        if(o instanceof String) {
+            return compareTo(new Version((String)o));
+        } else if (o instanceof Version) {
+            Version other = (Version)o;
+            if(variant != null && other.variant == null) return 1;
+            if(variant == null && other.variant != null) return -1;
+            if(variant != null && other.variant != null) {
+                if(!variant.equals(other.variant))
+                    return variant.compareTo(other.variant);
+            }
+            if(major!=other.major) return major - other.major;
+            if(minor!=other.minor) return minor - other.minor;
+            if(build!=other.build) return build - other.build;
+            return revision - other.revision;
+        } else {
+            throw new IllegalArgumentException("Can't compare Version to " + o.getClass().getCanonicalName());
+        }
+    }
+
+    public boolean lessThan(Object o) {
+        return compareTo(o) < 0;
+    }
+
+    public boolean greaterThan(Object o) {
+        return compareTo(o) > 0;
+    }
+
+    public boolean equalTo(Object o) {
+        return compareTo(o) == 0;
+    }
+}
---
+++ test/com/pclewis/mcpatcher/VersionTest.java
@@ -0,0 +1,67 @@
+package com.pclewis.mcpatcher;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class VersionTest {
+    @Test public void testCreateMajor() {
+        Version v = new Version("1");
+        Assert.assertEquals("1", v.toString());
+        Assert.assertEquals(null, v.variant );
+        Assert.assertEquals( 1, v.major   );
+        Assert.assertEquals(-1, v.minor   );
+        Assert.assertEquals(-1, v.build   );
+        Assert.assertEquals(-1, v.revision);
+    }
+
+    @Test public void testCreateMinor() {
+        Version v = new Version("1.4");
+        Assert.assertEquals("1.4", v.toString());
+        Assert.assertEquals(null, v.variant );
+        Assert.assertEquals( 1, v.major   );
+        Assert.assertEquals( 4, v.minor   );
+        Assert.assertEquals(-1, v.build   );
+        Assert.assertEquals(-1, v.revision);
+    }
+
+    @Test public void testCreateBuild() {
+        Version v = new Version("1.4.2");
+        Assert.assertEquals("1.4.2", v.toString());
+        Assert.assertEquals(null, v.variant );
+        Assert.assertEquals( 1, v.major   );
+        Assert.assertEquals( 4, v.minor   );
+        Assert.assertEquals( 2, v.build   );
+        Assert.assertEquals(-1, v.revision);
+    }
+
+    @Test public void testCreateRev() {
+        Version v = new Version("1.4.2_01");
+        Assert.assertEquals("1.4.2_1", v.toString()); // note 0 dropped
+        Assert.assertEquals(null, v.variant);
+        Assert.assertEquals(1, v.major   );
+        Assert.assertEquals(4, v.minor   );
+        Assert.assertEquals(2, v.build   );
+        Assert.assertEquals(1, v.revision);
+    }
+
+    @Test public void testCreateVariant() {
+        Version v = new Version("Alpha 1.4");
+        Assert.assertEquals("Alpha 1.4", v.toString());
+        Assert.assertEquals("Alpha", v.variant);
+        Assert.assertEquals(1, v.major   );
+        Assert.assertEquals(4, v.minor   );
+        Assert.assertEquals(-1, v.build   );
+        Assert.assertEquals(-1, v.revision);
+    }
+
+    @Test public void testCompare() {
+        Assert.assertTrue(new Version("1.2").compareTo("1.3") < 0);
+        Assert.assertTrue(new Version("1.2").compareTo("1.2.1") < 0);
+        Assert.assertTrue(new Version("1.2.1").compareTo("1.2.1_01") < 0);
+        Assert.assertTrue(new Version("1.2.1").compareTo("Server 1.2.1") < 0);
+        Assert.assertTrue(new Version("Alpha 1.2.1").compareTo("Server 1.2.1") < 0);
+        Assert.assertTrue(new Version("Server 4.8.12").compareTo("Alpha 9.18.27") > 0);
+        Assert.assertTrue(new Version("Alpha 1.2.16").compareTo("Alpha 1.2.3") > 0);
+        Assert.assertEquals(0, new Version("1.2.1_01").compareTo("1.2.1_01"));
+    }
+}
