99b8b7c44b33ff9f69e72488012a95b71fdad88e
Philip Lewis <pcl@pclewis.com>
2010-11-06 21:17:09 -0400

    hooked up log level and minecraft folder buttons

--- src/com/pclewis/mcpatcher/NewGui.java
+++ src/com/pclewis/mcpatcher/NewGui.java
@@ -8,8 +8,14 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class NewGui implements ActionListener {
+    private static Logger logger = Logger.getLogger("com.pclewis.mcpatcher.NewGui");
+
     private JPanel mainPanel;
     private JButton configureButton;
     private JButton uninstallButton;
@@ -20,12 +26,14 @@ public class NewGui implements ActionListener {
     private JButton checkForUpdateButton;
     private JPanel infoPanel;
     private JFrame frame;
+    private ButtonGroup logLevelButtonGroup;
 
     public NewGui() {
         this( new JFrame("Minecraft Patcher") );
         frame.setContentPane(this.getMainPanel());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
+        minecraftFolderButton.addActionListener(this);
     }
 
     public NewGui(JFrame frame) {
@@ -69,13 +77,26 @@ public class NewGui implements ActionListener {
     }
 
 
-
-    
-    private void addMenuItem( JMenu menu, String title, Icon icon, int mnemonic) {
+    private void addMenuItem(JMenu menu, String title, Icon icon, int mnemonic) {
         JMenuItem item = new JMenuItem(title, icon);
         item.setMnemonic(mnemonic);
         item.addActionListener(this);
         menu.add(item);
+    }
+
+    private void addRadioItem(JMenu menu, ButtonGroup bg, String title, String action, Icon icon, int mnemonic, boolean selected) {
+        JRadioButtonMenuItem item = new JRadioButtonMenuItem(title, icon);
+        item.setMnemonic(mnemonic);
+        item.setActionCommand(action);
+        item.addActionListener(this);
+        menu.add(item);
+        bg.add(item);
+        item.setSelected(selected);
+    }
+
+    private void addLogLevel(JMenu menu, ButtonGroup bg, Level level, int mnemonic) {
+        addRadioItem(menu, bg, level.getLocalizedName(), "log_level_" + level.getName(),
+                null, mnemonic, level == Level.SEVERE);
     }
 
     private void createMenu() {
@@ -92,6 +113,16 @@ public class NewGui implements ActionListener {
         menu.setMnemonic(KeyEvent.VK_A);
 
         addMenuItem(menu, "Create Moddable Jar", null, KeyEvent.VK_C );
+        JMenu subMenu = new JMenu("Log Level");
+        logLevelButtonGroup = new ButtonGroup();
+        addLogLevel(subMenu, logLevelButtonGroup, Level.SEVERE, KeyEvent.VK_S);
+        addLogLevel(subMenu, logLevelButtonGroup, Level.WARNING, KeyEvent.VK_W);
+        addLogLevel(subMenu, logLevelButtonGroup, Level.INFO, KeyEvent.VK_I);
+        addLogLevel(subMenu, logLevelButtonGroup, Level.CONFIG, KeyEvent.VK_C);
+        addLogLevel(subMenu, logLevelButtonGroup, Level.FINE, KeyEvent.VK_F);
+        addLogLevel(subMenu, logLevelButtonGroup, Level.FINER, KeyEvent.VK_R);
+        addLogLevel(subMenu, logLevelButtonGroup, Level.FINEST, KeyEvent.VK_T);
+        menu.add(subMenu);
         menuBar.add(menu);
 
         menu = new JMenu("Help");
@@ -107,9 +138,39 @@ public class NewGui implements ActionListener {
         frame.setJMenuBar(menuBar);
     }
 
+    private static void openMinecraftFolder() {
+        ProcessBuilder pb = new ProcessBuilder(Util.isWindows() ? "explorer" : "open",
+                new File(Util.getMinecraftPath()).getAbsolutePath());
+        try {
+            Process p = pb.start();
+        } catch(Exception e) {
+            logger.log(Level.SEVERE, "Can't open folder", e);
+        }
+    }
 
+    private static void runMinecraft() {
+        // FIXME
+    }
+
+    private void setLogLevel(Level level) {
+        for(Handler h : Logger.getLogger("").getHandlers()) {
+            h.setLevel(level);
+        }
+        Logger.getLogger("com.pclewis").setLevel(level);
+        logger.config("Set log level to " + level.getName());
+    }
 
     public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
 
+        if(command!=null) {
+            if(command.equals("mc_folder")) {
+                openMinecraftFolder();
+            } else if (command.equals("mc_run")) {
+                runMinecraft();
+            } else if (command.startsWith("log_level_")) {
+                setLogLevel( Level.parse(command.replace("log_level_", "")) );
+            }
+        }
     }
 }
