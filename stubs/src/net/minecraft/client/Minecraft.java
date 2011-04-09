300ff13d1593f0f135593bc0224ffcc44baf8596
Paul Rupe <prupe@nc.rr.com>
2011-04-09 11:50:07 -0400

    added imports

--- stubs/src/net/minecraft/client/Minecraft.java
+++ stubs/src/net/minecraft/client/Minecraft.java
@@ -1,5 +1,10 @@
 package net.minecraft.client;
 
+import net.minecraft.src.FontRenderer;
+import net.minecraft.src.GameSettings;
+import net.minecraft.src.RenderEngine;
+import net.minecraft.src.TexturePackList;
+
 public class Minecraft {
     public TexturePackList texturePackList;
     public RenderEngine renderEngine;
