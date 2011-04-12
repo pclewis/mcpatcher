15a2c54b27eba40c28de5bd62d6a87a0cef79249
Paul Rupe <prupe@nc.rr.com>
2011-04-12 19:19:01 -0400

    return proper INVOKEINTERFACE arguments

--- src/com/pclewis/mcpatcher/ConstPoolUtils.java
+++ src/com/pclewis/mcpatcher/ConstPoolUtils.java
@@ -3,6 +3,8 @@ package com.pclewis.mcpatcher;
 import javassist.bytecode.ConstPool;
 import javassist.bytecode.Mnemonic;
 
+import java.util.ArrayList;
+
 import static javassist.bytecode.Opcode.*;
 
 class ConstPoolUtils {
@@ -203,6 +205,7 @@ class ConstPoolUtils {
         if (index < 0) {
             return NOT_FOUND;
         }
+        opcode &= 0xff;
         switch (opcode) {
             case LDC:
             case LDC_W:
@@ -230,7 +233,8 @@ class ConstPoolUtils {
                 if (!(value instanceof InterfaceMethodRef)) {
                     throw new AssertionError(Mnemonic.OPCODE[opcode] + " requires an InterfaceMethodRef object");
                 }
-                break;
+                int numArgs = parseDescriptor(((InterfaceMethodRef) value).getType()).size();
+                return new byte[]{(byte) opcode, Util.b(index, 1), Util.b(index, 0), (byte) numArgs, 0};
 
             case INSTANCEOF:
             case CHECKCAST:
@@ -246,5 +250,27 @@ class ConstPoolUtils {
                 break;
         }
         return new byte[]{(byte) opcode, Util.b(index, 1), Util.b(index, 0)};
+    }
+
+    public static ArrayList<String> parseDescriptor(String desc) {
+        ArrayList<String> types = new ArrayList<String>();
+        desc = desc.replaceAll("[()]", "");
+        int len = desc.length();
+        int j;
+        for (int i = 0; i < len; i = j + 1) {
+            for (j = i; j < len; j++) {
+                char c = desc.charAt(j);
+                if ("SIFDZV".indexOf(c) >= 0) {
+                    break;
+                } else if (c == 'L') {
+                    while (desc.charAt(j) != ';') {
+                        j++;
+                    }
+                    break;
+                }
+            }
+            types.add(desc.substring(i, j + 1));
+        }
+        return types;
     }
 }
