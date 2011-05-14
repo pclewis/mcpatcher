20fb212162756a5e5505839cf68f36d1059714aa
Paul Rupe <prupe@nc.rr.com>
2011-05-14 17:24:29 -0400

    insert files into minecraft.jar

--- src/com/pclewis/mcpatcher/ExternalMod.java
+++ src/com/pclewis/mcpatcher/ExternalMod.java
@@ -1,6 +1,10 @@
 package com.pclewis.mcpatcher;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 class ExternalMod extends Mod {
@@ -13,5 +17,23 @@ class ExternalMod extends Mod {
 
         name = new File(zipFile.getName()).getName();
         description = String.format("Copy files from %s", prefix);
+
+        for (ZipEntry entry : Collections.list(zipFile.entries())) {
+            if (entry.isDirectory()) {
+                continue;
+            }
+            String name = entry.getName();
+            if (name.startsWith(prefix)) {
+                String suffix = name.substring(prefix.length());
+                filesToReplace.add(suffix);
+            }
+        }
+    }
+
+    @Override
+    public InputStream openFile(String filename) throws IOException {
+        String path = prefix + filename.replaceFirst("^/", "");
+        Logger.log(Logger.LOG_CLASS, "opening %s (%s + %s)", path, prefix, filename);
+        return zipFile.getInputStream(new ZipEntry(path));
     }
 }
