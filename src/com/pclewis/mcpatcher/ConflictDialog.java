49e9b2e5fbf3b80e8299edd74b1979b443316eb1
Paul Rupe <prupe@nc.rr.com>
2011-10-02 14:44:48 -0400

    sort mod conflict groups alphabetically

--- src/com/pclewis/mcpatcher/ConflictDialog.java
+++ src/com/pclewis/mcpatcher/ConflictDialog.java
@@ -1,29 +1,42 @@
 package com.pclewis.mcpatcher;
 
 import javax.swing.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 class ConflictDialog {
     private JPanel contentPane;
     private JTextArea conflictsText;
 
     ConflictDialog(HashMap<String, ArrayList<Mod>> conflicts) {
-        HashMap<ArrayList<Mod>, ArrayList<String>> c = new HashMap<ArrayList<Mod>, ArrayList<String>>();
+        HashMap<ArrayList<Mod>, ArrayList<String>> conflictGroups = new HashMap<ArrayList<Mod>, ArrayList<String>>();
         for (Map.Entry<String, ArrayList<Mod>> entry : conflicts.entrySet()) {
             String filename = entry.getKey();
             ArrayList<Mod> mods = entry.getValue();
-            ArrayList<String> fileList = c.get(mods);
+            ArrayList<String> fileList = conflictGroups.get(mods);
             if (fileList == null) {
                 fileList = new ArrayList<String>();
-                c.put(mods, fileList);
+                conflictGroups.put(mods, fileList);
             }
             fileList.add(filename);
         }
         StringBuilder message = new StringBuilder();
-        for (Map.Entry<ArrayList<Mod>, ArrayList<String>> entry : c.entrySet()) {
+        ArrayList<Map.Entry<ArrayList<Mod>, ArrayList<String>>> conflictEntries = new ArrayList<Map.Entry<ArrayList<Mod>, ArrayList<String>>>();
+        conflictEntries.addAll(conflictGroups.entrySet());
+        Collections.sort(conflictEntries, new Comparator<Map.Entry<ArrayList<Mod>, ArrayList<String>>>() {
+            public int compare(Map.Entry<ArrayList<Mod>, ArrayList<String>> o1, Map.Entry<ArrayList<Mod>, ArrayList<String>> o2) {
+                ArrayList<Mod> a1 = o1.getKey();
+                ArrayList<Mod> a2 = o2.getKey();
+                for (int i = 0; i < a1.size() && i < a2.size(); i++) {
+                    Mod mod1 = a1.get(i);
+                    Mod mod2 = a2.get(i);
+                    if (mod1 != mod2) {
+                        return mod1.getName().compareTo(mod2.getName());
+                    }
+                }
+                return a1.size() - a2.size();
+            }
+        });
+        for (Map.Entry<ArrayList<Mod>, ArrayList<String>> entry : conflictEntries) {
             ArrayList<Mod> mods = entry.getKey();
             ArrayList<String> filenames = entry.getValue();
             Collections.sort(filenames);
