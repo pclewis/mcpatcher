9da8cb1b34b7e57d02aea218fe9886fef0b4b71e
Paul Rupe <prupe@nc.rr.com>
2011-10-14 21:54:29 -0400

    allow custom animations from items.png

--- newcode/src/com/pclewis/mcpatcher/mod/CustomAnimation.java
+++ newcode/src/com/pclewis/mcpatcher/mod/CustomAnimation.java
@@ -30,19 +30,23 @@ public class CustomAnimation extends TextureFX {
         this.isScrolling = (minScrollDelay >= 0);
 
         BufferedImage custom = null;
-        String customSrc = "custom_" + name + ".png";
+        String imageName = tileImage == 0 ? "/terrain.png" : "/gui/items.png";
         try {
-            custom = TextureUtils.getResourceAsBufferedImage("/" + customSrc);
+            String customSrc = "/custom_" + name + ".png";
+            custom = TextureUtils.getResourceAsBufferedImage(customSrc);
+            if (custom != null) {
+                imageName = customSrc;
+            }
         } catch (IOException ex) {
         }
         MCPatcherUtils.log("new CustomAnimation %s, src=%s, buffer size=0x%x, tile=%d",
-            name, (custom == null ? "terrain.png" : customSrc), imageData.length, this.tileNumber
+            name, imageName, imageData.length, this.tileNumber
         );
 
         if (custom == null) {
             BufferedImage tiles;
             try {
-                tiles = TextureUtils.getResourceAsBufferedImage("/terrain.png");
+                tiles = TextureUtils.getResourceAsBufferedImage(imageName);
             } catch (IOException e) {
                 e.printStackTrace();
                 return;
@@ -60,18 +64,18 @@ public class CustomAnimation extends TextureFX {
             numFrames = custom.getHeight() / custom.getWidth();
             int imageBuf[] = new int[custom.getWidth() * custom.getHeight()];
             custom.getRGB(0, 0, custom.getWidth(), custom.getHeight(), imageBuf, 0, TileSize.int_size);
-            this.src = new byte[imageBuf.length * 4];
-            ARGBtoRGBA(imageBuf, this.src);
+            src = new byte[imageBuf.length * 4];
+            ARGBtoRGBA(imageBuf, src);
         }
     }
 
     static private void ARGBtoRGBA(int[] src, byte[] dest) {
         for (int i = 0; i < src.length; ++i) {
             int v = src[i];
-            dest[(i * 4) + 3] = (byte) ((v >> 24) & 0xFF);
-            dest[(i * 4) + 0] = (byte) ((v >> 16) & 0xFF);
-            dest[(i * 4) + 1] = (byte) ((v >> 8) & 0xFF);
-            dest[(i * 4) + 2] = (byte) ((v >> 0) & 0xFF);
+            dest[(i * 4) + 3] = (byte) ((v >> 24) & 0xff);
+            dest[(i * 4) + 0] = (byte) ((v >> 16) & 0xff);
+            dest[(i * 4) + 1] = (byte) ((v >> 8) & 0xff);
+            dest[(i * 4) + 2] = (byte) ((v >> 0) & 0xff);
         }
     }
 
