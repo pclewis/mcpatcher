889d678fc8a506c5210d21a2081a1f68bc48c548
Paul Rupe <prupe@nc.rr.com>
2011-10-15 10:51:12 -0400

    improve testfilter

---
+++ testfilter.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -p
+
+use strict;
+
+s/-> [a-z]+ @[[:digit:]]+/-> xx @.../g if /(method|field) ref/ || /string replace/;
+s/-> [a-z\/M]+\.[a-zA-Z<>]+ /-> xx.x /g if /(method|field) ref/;
+s/@[[:digit:]]+/@.../g;
+s/((INVOKE(VIRTUAL|STATIC|INTERFACE|SPECIAL))|((GET|PUT)(FIELD|STATIC))|NEW|IF_?[A-Z]+)( 0x[[:xdigit:]]{2}){2}/$1 0x.. 0x../g;
+s/LDC 0x[[:xdigit:]]{2}/LDC 0x../g;
+s/^(OS|JVM|Classpath): .*/$1: .../;
+s/\b[a-z]+\.class/xx.class/g;
+s/L[a-z]+;/Lxx;/g;
+s/ [a-z]+/ xx/g if /multiple matches: /;
+s/-> instruction \d+/-> instruction .../;
