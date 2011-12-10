08fed761f90a1a2f7f6b8a8e25c03c5e7a1e05a7
Paul Rupe <prupe@nc.rr.com>
2011-12-09 22:17:39 -0500

    added OrSignature

---
+++ src/com/pclewis/mcpatcher/OrSignature.java
@@ -0,0 +1,30 @@
+package com.pclewis.mcpatcher;
+
+import javassist.bytecode.ClassFile;
+
+public class OrSignature extends ClassSignature {
+    private ClassSignature[] signatures;
+    private ClassSignature matchedSignature;
+
+    public OrSignature(ClassSignature... signatures) {
+        this.signatures = signatures;
+    }
+
+    @Override
+    public boolean match(String filename, ClassFile classFile, ClassMap tempClassMap) {
+        for (ClassSignature signature : signatures) {
+            if (signature.match(filename, classFile, tempClassMap)) {
+                matchedSignature = signature;
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    @Override
+    public void afterMatch(ClassFile classFile) {
+        if (matchedSignature != null) {
+            matchedSignature.afterMatch(classFile);
+        }
+    }
+}
