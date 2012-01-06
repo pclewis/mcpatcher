4e73c9d48a60794f461bd57d05f10b8a305c6081
Paul Rupe <prupe@nc.rr.com>
2012-01-05 19:35:20 -0500

    filter INSTANCEOF arguments

--- testfilter.pl
+++ testfilter.pl
@@ -5,7 +5,7 @@ use strict;
 s/-> [a-z]+ @[[:digit:]]+/-> xx @.../g if /(method|field) ref/ || /string replace/;
 s/-> [a-z\/M]+\.[a-zA-Z<>]+ /-> xx.x /g if /(method|field) ref/;
 s/@[[:digit:]]+/@.../g;
-s/((INVOKE(VIRTUAL|STATIC|INTERFACE|SPECIAL))|((GET|PUT)(FIELD|STATIC))|NEW|CHECKCAST|IF_?[A-Z]+)( 0x[[:xdigit:]]{2}){2}/$1 0x.. 0x../g;
+s/((INVOKE(VIRTUAL|STATIC|INTERFACE|SPECIAL))|((GET|PUT)(FIELD|STATIC))|NEW|CHECKCAST|INSTANCEOF|IF_?[A-Z]+)( 0x[[:xdigit:]]{2}){2}/$1 0x.. 0x../g;
 s/LDC 0x[[:xdigit:]]{2}/LDC 0x../g;
 s/^(OS|JVM|Classpath): .*/$1: .../;
 s/\b[a-z]+\.class/xx.class/g;
