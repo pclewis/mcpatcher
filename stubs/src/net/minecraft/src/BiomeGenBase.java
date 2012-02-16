12e2b5c9fbdb0fd9efe7cefb424eeecefa43fd50
Paul Rupe <prupe@nc.rr.com>
2012-02-15 21:17:44 -0500

    updated stubs

--- stubs/src/net/minecraft/src/BiomeGenBase.java
+++ stubs/src/net/minecraft/src/BiomeGenBase.java
@@ -8,11 +8,11 @@ public class BiomeGenBase {
     public float rainfall;
     public int waterColorMultiplier;
     
-    public int getTemperaturef() {
+    public int getTemperaturef() { // 1.2 and up
         return 0;
     }
     
-    public int getRainfallf() {
+    public int getRainfallf() { // 1.2 and up
         return 0;
     }
 }
