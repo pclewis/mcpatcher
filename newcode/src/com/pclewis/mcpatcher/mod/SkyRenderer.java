b5f887f7d091977e5d161956c12acdc291fcc2a5
Paul Rupe <prupe@nc.rr.com>
2012-07-03 17:32:08 -0400

    playing around with shaders

--- newcode/src/com/pclewis/mcpatcher/mod/SkyRenderer.java
+++ newcode/src/com/pclewis/mcpatcher/mod/SkyRenderer.java
@@ -5,6 +5,9 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.src.*;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.util.glu.GLU;
 
 import java.util.HashMap;
 
@@ -21,6 +24,43 @@ public class SkyRenderer {
     private static final HashMap<Integer, Boolean> haveSkyBox = new HashMap<Integer, Boolean>();
     private static TexturePackBase lastTexturePack;
 
+    private static final int shaderProgram;
+    private static final int fragShader;
+    private static final int texture1Location;
+    private static final int texture2Location;
+    private static final int blendLocation;
+
+    private static final int SKY_TEXTURE_UNIT = 2;
+    private static final int STAR_TEXTURE_UNIT = SKY_TEXTURE_UNIT + 1;
+    private static final String SHADER_SOURCE =
+        "/* Simple shader to blend two textures */\n" +
+            "uniform sampler2D texture1;\n" +
+            "uniform sampler2D texture2;\n" +
+            "uniform float blending;\n" +
+            "void main() {\n" +
+            "    vec4 color1 = texture2D(texture1, gl_TexCoord[0].st);\n" +
+            "    vec4 color2 = texture2D(texture2, gl_TexCoord[0].st);\n" +
+            "    gl_FragColor = (1.0 - blending) * color1 + blending * color2;\n" +
+            "}\n";
+
+    static {
+        shaderProgram = GL20.glCreateProgram();
+        fragShader = GL20.glCreateShader(GL20.GL_FRAGMENT_SHADER);
+        GL20.glShaderSource(fragShader, SHADER_SOURCE);
+        GL20.glCompileShader(fragShader);
+        GL20.glAttachShader(shaderProgram, fragShader);
+        GL20.glLinkProgram(shaderProgram);
+
+        texture1Location = GL20.glGetUniformLocation(shaderProgram, "texture1");
+        texture2Location = GL20.glGetUniformLocation(shaderProgram, "texture2");
+        blendLocation = GL20.glGetUniformLocation(shaderProgram, "blending");
+
+        MCPatcherUtils.info(
+            "shaderProgram = %d, fragShader = %d, blendLocation = %d, texture1Location = %d, texture2Location = %d",
+            shaderProgram, fragShader, blendLocation, texture1Location, texture2Location
+        );
+    }
+
     public static void setup(World world, RenderEngine renderEngine, float partialTick) {
         Minecraft minecraft = MCPatcherUtils.getMinecraft();
         TexturePackBase texturePack = minecraft.texturePackList.getSelectedTexturePack();
@@ -28,13 +68,13 @@ public class SkyRenderer {
             lastTexturePack = texturePack;
             haveSkyBox.clear();
         }
-        if (texturePack instanceof TexturePackDefault || Keyboard.isKeyDown(Keyboard.KEY_ADD)) {
+        if (texturePack instanceof TexturePackDefault || Keyboard.isKeyDown(Keyboard.KEY_ADD) || shaderProgram <= 0 || fragShader <= 0) {
             active = false;
         } else {
             worldType = minecraft.getWorld().worldProvider.worldType;
             Boolean h = haveSkyBox.get(worldType);
             if (h == null) {
-                h = MCPatcherUtils.readImage(texturePack.getInputStream(getSkyTexture())) != null;
+                h = hasTexture(getSkyTexture()) & hasTexture(getStarTexture());
                 haveSkyBox.put(worldType, h);
             }
             active = h;
@@ -57,7 +97,18 @@ public class SkyRenderer {
             GL11.glDepthMask(false);
             GL11.glEnable(GL11.GL_TEXTURE_2D);
 
+            GL13.glActiveTexture(GL13.GL_TEXTURE0 + SKY_TEXTURE_UNIT);
             renderEngine.bindTexture(renderEngine.getTexture(getSkyTexture()));
+            GL13.glActiveTexture(GL13.GL_TEXTURE0 + STAR_TEXTURE_UNIT);
+            renderEngine.bindTexture(renderEngine.getTexture(getStarTexture()));
+
+            GL20.glUseProgram(shaderProgram);
+
+            GL20.glUniform1i(texture1Location, SKY_TEXTURE_UNIT);
+            GL20.glUniform1i(texture2Location, STAR_TEXTURE_UNIT);
+            GL20.glUniform1f(blendLocation, celestialAngle);
+
+            //renderEngine.bindTexture(renderEngine.getTexture(getSkyTexture()));
 
             GL11.glPushMatrix();
 
@@ -92,6 +143,9 @@ public class SkyRenderer {
 
             GL11.glPopMatrix();
 
+            GL20.glUseProgram(0);
+            GL13.glActiveTexture(GL13.GL_TEXTURE0);
+
             GL11.glDepthMask(true);
             GL11.glEnable(GL11.GL_ALPHA_TEST);
             GL11.glEnable(GL11.GL_TEXTURE_2D);
@@ -115,7 +169,28 @@ public class SkyRenderer {
         tessellator.draw();
     }
 
+    private static boolean hasTexture(String s) {
+        if (MCPatcherUtils.readImage(lastTexturePack.getInputStream(s)) == null) {
+            MCPatcherUtils.info("texture %s NOT FOUND", s);
+            return false;
+        } else {
+            MCPatcherUtils.info("texture %s FOUND", s);
+            return true;
+        }
+    }
+
     private static String getSkyTexture() {
         return "/terrain/world" + worldType + "/sky.png";
+    }
+
+    private static String getStarTexture() {
+        return "/terrain/world" + worldType + "/stars.png";
+    }
+
+    private static void checkGLError() {
+        int error = GL11.glGetError();
+        if (error != 0) {
+            throw new RuntimeException("GL error: " + GLU.gluErrorString(error));
+        }
     }
 }
