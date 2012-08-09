3f0f7f8526a2137460689d7d00fd17c0851db647
Paul Rupe <prupe@nc.rr.com>
2012-08-09 19:05:43 -0400

    improved test filter

--- testfilter.pl
+++ testfilter.pl
@@ -2,13 +2,20 @@
 
 use strict;
 
-s/-> [a-z]+ @[[:digit:]]+/-> xx @.../g if /(method|field) ref/ || /string replace/;
-s/-> [a-z\/M]+\.[a-zA-Z<>]+ /-> xx.x /g if /(method|field) ref/;
-s/@[[:digit:]]+/@.../g;
-s/((INVOKE(VIRTUAL|STATIC|INTERFACE|SPECIAL))|((GET|PUT)(FIELD|STATIC))|NEW|CHECKCAST|INSTANCEOF|IF_?[A-Z]+)( 0x[[:xdigit:]]{2}){2}/$1 0x.. 0x../g;
-s/LDC 0x[[:xdigit:]]{2}/LDC 0x../g;
-s/^(OS|JVM|Classpath): .*/$1: .../;
 s/\b[a-z]+\.class/xx.class/g;
 s/L[a-z]+;/Lxx;/g;
 s/ [a-z]+/ xx/g if /multiple matches: /;
+s/\b[a-z]+(\(\S*\))/xx$1/g;
+
+s/(matches|->) [a-zA-Z_]{1,3}\b/$1 xx/g if /field|string replace/;
+s/(matches|->) [a-zA-Z_]{1,3} (\w*\(\S*\)\S+)/$1 xx $2/g if /method|class ref/;
+s/-> [a-z\/M]+\.[a-zA-Z<>]+ /-> xx.x /g if /(class|method|field) ref/;
+
+s/@[[:digit:]]+/@.../g;
 s/-> instruction \d+/-> instruction .../;
+
+s/((INVOKE(VIRTUAL|STATIC|INTERFACE|SPECIAL))|((GET|PUT)(FIELD|STATIC))|NEW|CHECKCAST|INSTANCEOF|IF_?[A-Z]+)( 0x[[:xdigit:]]{2}){2}/$1 0x.. 0x../g;
+s/LDC 0x[[:xdigit:]]{2}/LDC 0x../g;
+s/(LDC2?_W) 0x[[:xdigit:]]{2} 0x[[:xdigit:]]{2}/$1 0x.. 0x../g;
+
+s/^(OS|JVM|Classpath): .*/$1: .../;
