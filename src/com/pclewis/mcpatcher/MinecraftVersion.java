7c2e471c618e0e20375c0944cabfdd64f5c85add
Paul Rupe <prupe@nc.rr.com>
2012-11-16 18:25:02 -0500

    md5sum for 1.4.5

--- src/com/pclewis/mcpatcher/MinecraftVersion.java
+++ src/com/pclewis/mcpatcher/MinecraftVersion.java
@@ -154,6 +154,7 @@ final public class MinecraftVersion {
             addKnownVersion("1.4.2", "771175c01778ea67395bc6919a5a9dc5");
             addKnownVersion("1.4.3", "9cc3295931edb6339f22989fe1b612a6");
             addKnownVersion("1.4.4", "7aa46c8058cba2f38e9d2ddddcc77c72");
+            addKnownVersion("1.4.5", "469c9743ba88b7aa498769db75e31b1c");
 
             for (int i = 0; i < versionOrdering.size(); i++) {
                 MinecraftVersion a = versionOrdering.get(i);
